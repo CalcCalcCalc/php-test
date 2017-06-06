a69d00dfa4c17cdd53d95e467d0f12172962e9eb
CalcCalcCalc <alecdsstone@gmail.com>
2017-06-06 17:47:42 +0100

    testing php variables6

--- index.php
+++ index.php
@@ -14,6 +14,18 @@ $string1 = "First string";
 $string2 = "Second string";
 echo "<p>".$string1.". ".$string2."</p>";
 
+//calculation is simple
+$myNumber = 42;
+$calculation = $myNumber * 31 / 55 * 3;
+echo "<p>The result of the calculation is ".$calculation."</p>";
 
+//booleans are either 1 (true) or " " (false)
+$trueBool = true;
+$falseBool = false;
+echo "<p>True looks like this: ".$trueBool.", and false looks like this: ".$falseBool;
+
+//We can nest variables, but is not common
+$nestedVariable = "name";
+echo $$nestedVariable;
 
 ?>
