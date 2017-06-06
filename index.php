f7a70a846d1d511d74a3445af625dd55fb280f55
CalcCalcCalc <alecdsstone@gmail.com>
2017-06-06 21:19:18 +0100

    php lessons post 4?

--- index.php
+++ index.php
@@ -2,13 +2,28 @@
 
 <?php
 
-  print_r($_POST)
+  $users = array("Alex" => "abc123", "Jeff" => "xyz987");
+
+  $name = $_POST["name"];
+  $password = $_POST["password"];
+
+  if ($_POST){
+    foreach($users as $key => $value){
+      if (($key == $name) && ($value == $password)){
+        echo "<p>Welcome ".$name."</p>";
+        break;
+      }
+    }
+    echo "<p>Please enter a correct username an password</p>";
+  }
 
 ?>
 
 <form method="post">
 
-  <input name="number" type="text">
+  <input name="name" type="text">
+
+  <input name="password" type="text">
 
   <input type="submit" value="Go!">
 
