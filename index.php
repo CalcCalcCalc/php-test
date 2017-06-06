5f119bd3235e6a95198cd90d1ea0a745cca59ee5
CalcCalcCalc <alecdsstone@gmail.com>
2017-06-06 17:37:13 +0100

    testing php variables4

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <?php
 
+//declare variables with "$"
 $name = "Alec";
 
+// call variables with "$variableName"
 echo $name;
 
+// variables are parsed inside strings
 echo "My name is $name";
 
+// Strings can be concatinated using "."
 $string1 = "First string";
+$string2 = "Second string";
+echo $string1.". ".$string2;
 
-$string2 = ", second string";
 
-echo $string1.$string2;
 
 ?>
