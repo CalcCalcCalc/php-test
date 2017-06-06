641899ff899121fb462795a70e8eb163226ec6b4
CalcCalcCalc <alecdsstone@gmail.com>
2017-06-06 17:54:19 +0100

    testing php variables9

--- index.php
+++ index.php
@@ -7,17 +7,17 @@ $name = "Alec";
 echo $name;
 
 // variables are parsed inside strings
-echo "</br><p>My name is $name</p>";
+echo "</br><p>My name is a variable inside a string and that name is $name</p>";
 
 // Strings can be concatinated using "."
-$string1 = "First string";
-$string2 = "Second string";
+$string1 = "<span style="color: red">This is one string</span>";
+$string2 = "<span style="color: green">concatinated with a second string<span>";
 echo "<p>".$string1.". ".$string2."</p>";
 
 //calculation is simple
 $myNumber = 42;
 $calculation = $myNumber * 31 / 55 * 3;
-echo "<p>The result of the calculation is ".$calculation."</p>";
+echo "<p>The result of the variable calculation is ".$calculation."</p>";
 
 //booleans are either 1 (true) or " " (false)
 $trueBool = true;
@@ -26,6 +26,6 @@ echo "<p>True looks like this: ".$trueBool.", and false looks like this: ".$fals
 
 //We can nest variables, but is not common
 $nestedVariable = "name";
-echo "<p>".$$nestedVariable."</p>";
+echo "<p>My name is now a nested variable, and that name is ".$$nestedVariable."</p>";
 
 ?>
