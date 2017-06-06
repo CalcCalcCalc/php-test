7a538a6311eef4f0f4d138541e99579ca1d6c80d
CalcCalcCalc <alecdsstone@gmail.com>
2017-06-06 18:42:08 +0100

    testing php forLoops 6

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $family = array("Howard", "Beth", "Jim", "Christine");
 
 foreach ($family as $key => $value) {
 
-  $family($key) = $value." Smith";
+  $family[$key] = $value." Smith";
 
   echo $key." ".$value." </br>";
 }
