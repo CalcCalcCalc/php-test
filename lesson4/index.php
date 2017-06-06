c5ae6634d80b2e607d4b2e86b2884282c8f0c0b0
CalcCalcCalc <alecdsstone@gmail.com>
2017-06-06 20:11:52 +0100

    php lesson primes

---
+++ lesson4/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$family = array("Howard", "Beth", "Jim", "Christine");
+
+foreach ($family as $key => $value) {
+
+  $family[$key] = $value." Smith";
+
+  echo $family[$key]." </br>";
+}
+
+?>
