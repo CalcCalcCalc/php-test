43d0f570b0e653ba63ed947f79aacb0731de797e
CalcCalcCalc <alecdsstone@gmail.com>
2017-06-06 18:36:59 +0100

    testing php forLoops 3

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $family = array("Howard", "Beth", "Jim", "Christine");
 
 foreach ($family as $key => $value) {
-  ehco "List item ".$key." is ".$value." ";
+  echo "List item ".$key." is ".$value." ";
 }
 
 ?>
