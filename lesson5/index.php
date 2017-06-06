6a578fb1258c272b8f2d998ccf31b00a405c4edc
CalcCalcCalc <alecdsstone@gmail.com>
2017-06-06 21:08:08 +0100

    php lesson post 1

--- lesson5/index.php
+++ lesson5/index.php
@@ -1,15 +1,42 @@
+<!-- <p>Prime number validator</p> -->
+
+<form>
+
+  <input name="number" type="text">
+
+  <input type="submit" value="Go!">
+
+</form>
+
+
 <?php
 
-$i = 1;
+  if ($_GET["number"] == false){
+    echo "<p>Please enter a number.<p/>";
+  } else {
+    $num = $_GET["number"];
+    $trueText = "<p>".$num." is prime.</p>";
+    $falseText = "<p>".$num." is NOT prime.</p>";
+    if ($num <= 1){
+      echo $falseText;
+      return;
+    } elseif ($num <= 3){
+      echo $trueText;
+      return;
+    } elseif (($num % 2 == 0) || ($num % 3 == 0)){
+      echo $falseText;
+      return;
+    }
+    $i = 5;
+    while ($i * $i <= $num){
+      if (($num % i == 0) || ($num % ($i + 2) == 0)){
+        echo $falseText;
+        return;
+      }
+      $i = $i + 6;
+    }
+    echo $trueText;
 
-while ($i <= 10) {
+  }
 
-  $j = $i * 5;
-
-  echo $j."</br>";
-
-  $i++;
-
-}
-
-?>
+ ?>
