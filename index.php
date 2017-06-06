2cbea734b99e301f6f8ffdf0168e60f6a6dd05c7
CalcCalcCalc <alecdsstone@gmail.com>
2017-06-06 21:27:57 +0100

    php lessons email 1

--- index.php
+++ index.php
@@ -1,31 +1,17 @@
-<!-- <p>Prime number validator</p> -->
-
-<form method="post">
-
-  <input name="name" type="text">
-
-  <input name="password" type="text">
-
-  <input type="submit" value="Go!">
-
-</form>
-
-
 <?php
 
-  $users = array("Alex" => "abc123", "Jeff" => "xyz987");
+  $emailto = "alecdsstone@gmail.com";
 
-  $name = $_POST["name"];
-  $password = $_POST["password"];
+  $subject = "I hope this works!";
 
-  if ($_POST){
-    foreach($users as $key => $value){
-      if (($key == $name) && ($value == $password)){
-        echo "<p>Welcome ".$name."</p>";
-        return;
-      }
-    }
-    echo "<p>Please enter a correct username an password</p>";
+  $body = "I think you're awesome";
+
+  $headers = "From: alecdsstone@gmail.com";
+
+  if (mail($emailto, $subject, $body, $headers)){
+    echo "The email was sent successfully!";
+  } else {
+    echo "The email could not be sent.";
   }
 
-?>
+ ?>
